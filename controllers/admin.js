82ac547a130e6c591e88df96eb45da12a698f8d5
soheil <solyacount@gmail.com>
2025-12-18 10:46:37 +0330

    add get admin product

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,23 +1,44 @@
 const Product = require('../models/product');
-exports.getAddProduct = (req,res)=>{
-    res.render('admin/add_product',{path:'/admin/add_product',pageTitle:'add product'})
+exports.getAddProduct = (req, res) => {
+    res.render('admin/add_product',
+    { path: '/admin/add_product', pageTitle: 'add product' })
 };
 
-exports.postAddProduct= async(req,res)=>{
+//the page that admin go to edit products
+exports.getAdminProduct = async (req, res) => {
+    try {
+        const products = await Product.find()
+        res.render('admin/edit-products', {
+            prods: products,
+            pageTitle: 'admin products',
+            path: 'admin/edit-products'
+        });
+    } catch (err) {
+        console.log(err.message);
+    }
+
+}
+
+//post product page action
+exports.postAddProduct = async (req, res) => {
     //import values 
     const title = req.body.title;
     const price = req.body.price;
     const imageUrl = req.body.imageUrl;
-    const description= req.body.description;
+    const description = req.body.description;
     // creat product 
     const product = new Product({
         title: title,
-        price : price ,
+        price: price,
         imageUrl: imageUrl,
-        description:description
+        description: description
     });
     await product.save();
     console.log("created new product...");
     res.redirect('/');
-    
+
 }
+
+
+
+
--- routes/admin.js
+++ routes/admin.js
@@ -4,5 +4,6 @@ const adminController = require('../controllers/admin');
 
 router.get('/add_product',adminController.getAddProduct);
 router.post('/add_product',adminController.postAddProduct);
+router.get('/edit-products',adminController.getAdminProduct);
 
 module.exports=router;
