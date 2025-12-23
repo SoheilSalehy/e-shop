930e38da17eb71ab8181ca1da2a62614c0169a21
soheil <solyacount@gmail.com>
2025-12-23 17:54:55 +0330

    hichiiii

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser'); 
+const bodyParser = require('body-parser');
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 const app = express();
 const mongoose = require('mongoose');
-//const user = require('./models/user')
+const User = require('./models/user')
 const PORT = 3000;
 
 //=== port ===
@@ -14,25 +14,38 @@ app.listen(PORT, () => {
 });
 
 //=== middleware ===
-app.use(bodyParser.urlencoded({extended:false}));
-app.use('/admin',adminRouter);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/admin', adminRouter);
 app.use(shopRouter);
 app.use(express.static(path.join(__dirname, 'public')));
 
 //=== set ===
-app.set('view engine','ejs'); // set ejs render for template engine
+app.set('view engine', 'ejs'); // set ejs render for template engine
 app.set('views', 'views'); // to set the address of views file
 
 // listen to port 
 mongoose.connect('mongodb://localhost/shop')
-.then(result=>{
+    .then(result => {
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'soheil',
+                    email: 'solyacount@gmail.com',
+                    cart:{
+                        item:[]
+                    }
+                    
+                    
+                });
+                //694a9e3f9a6cf83d1421527d
+            }
+        })
 
-
-    app.listen(PORT,()=>{
-        console.log('listening on port ',PORT);
+        app.listen(PORT, () => {
+            console.log('listening on port ', PORT);
+        })
     })
-})
-.catch(err=>{
-    console.log("Error : ",err.message);
-});
+    .catch(err => {
+        console.log("Error : ", err.message);
+    });
 
